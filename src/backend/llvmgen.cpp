fb0f1b5b36f3c8911b93f6836625ba66ae2bcb66
harshaneo17 <harsha.god86@gmail.com>
2024-04-24 20:20:08 +0100

    added changes to llvmgen

--- src/backend/llvmgen.cpp
+++ src/backend/llvmgen.cpp
@@ -7,14 +7,19 @@
 #include <llvm/IR/Verifier.h>
 #include <llvm/Support/raw_ostream.h>
 #include <llvm/IR/DerivedTypes.h>
+#include <nlohmann/json.hpp>
 #include <map>
 #include <vector>
 #include <string>
 #include <stdexcept>
 
+using json = nlohmann::json;
+
 class LLVMCodeGenerator {
 public:
-    LLVMCodeGenerator() : module(std::make_unique<llvm::Module>("Bril_Module", context)) {}
+    LLVMCodeGenerator() : module("Bril_Module", context) {}
+
+    llvm::Module module;
 
     void generate(const json& bril_prog) {
         for (auto& fn : bril_prog["functions"]) {
@@ -24,12 +29,11 @@ public:
 
 private:
     llvm::LLVMContext context;
-    std::unique_ptr<llvm::Module> module;
     llvm::IRBuilder<> builder{ context };
-    std::map<std::string, llvm::Value> func_symtab;
-    std::map<std::string, llvm::BasicBlock> func_bbs;
+    std::map<std::string, llvm::Value*> func_symtab;
+    std::map<std::string, llvm::BasicBlock*> func_bbs;
 
-    llvm::Type gen_type(const std::string& type) {
+    llvm::Type* gen_type(const std::string& type) {
         if (type == "int") {
             return llvm::Type::getInt32Ty(context);
         }
@@ -40,27 +44,41 @@ private:
             return llvm::Type::getInt1Ty(context);
         }
         else {
-            throw std::runtime_error("Unknown type: " + type);
+            throw std::runtime_error("Unknown type " + type);
         }
     }
 
-    std::shared_ptr<llvm::Value> gen_var(const std::string& var) {
+    llvm::Value* gen_var(const std::string& var) {
         auto it = func_symtab.find(var);
         if (it != func_symtab.end()) {
-            return it.second;
+            return it->second;
         }
         else {
-            throw std::runtime_error("Unknown variable: " + var); 
+            throw std::runtime_error("Unknown variable " + var);
         }
     }
+
+    llvm::BasicBlock* gen_label(const std::string& label) {
+        auto it = func_bbs.find(label);
+        if (it != func_bbs.end()) {
+            return it->second;
+        }
+        else {
+            auto* bb = llvm::BasicBlock::Create(context, label, builder.GetInsertBlock()->getParent());
+            func_bbs[label] = bb;
+            return bb;
+        }
+    }
+
 
 };
 
 int main() {
     LLVMCodeGenerator code_gen;
     json bril_prog;
+    std::cout << "Enter the json file" << std::endl;
     std::cin >> bril_prog;
     code_gen.generate(bril_prog);
-    code_gen.module->print(llvm::outs(), nullptr);
+    code_gen.module.print(llvm::outs(), nullptr);
     return 0;
 }
